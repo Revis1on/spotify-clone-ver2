6df842e4c1a7bc76b8eb1559aa7b8341229bca53
Filip Risteski <filip22risteskI@gmail.com>
2021-12-31 17:51:05 +0100

    ver 1.0

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistIdState = atom({
+  key: "playlistIdList",
+  default: "3eKwcW6N6hujMoUhPzBIKR",
+});
+
+export const playlistState = atom({
+  key: "playlistState",
+  default: null,
+});
